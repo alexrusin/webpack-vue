d1aad70e4f4c292604b14cd016a70ba3c8412033
Alex Rusin <alexrusin_2000@yahoo.com>
2017-09-30 14:41:51 -0700

    vue-loader

---
+++ vue-loader-app/hello-vue/src/components/Message.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <div class="box">
+
+	<p>
+    	<slot></slot>
+    </p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'message',
+  data () {
+    return {
+     
+    }
+  }
+}
+
+</script>
+
+<style>
+
+.box {
+	background-color: #ccc;
+	padding: 10px;
+	border: 3px solid #000;
+	margin-bottom: 20px;
+}
+
+</style>
